483cc6da9f7ea313eb869373eeb52c1055364dc5
michaelkalin1 <68094332+michaelkalin1@users.noreply.github.com>
2022-04-08 15:29:41 -0400

    added better interrupt handling

--- take_pic.py
+++ take_pic.py
@@ -1,3 +1,4 @@
+from email.contentmanager import ContentManager
 from picamera import PiCamera
 from time import sleep
 import os
@@ -11,23 +12,29 @@ camera.resolution = (320, 240)
 i = 0
 
 while(1):
-    camera.capture('wally.jpg')
+    try:
+        camera.capture('wally.jpg')
 
-    sleep(1)
+        sleep(1)
 
-    os.system(f'cp wally.jpg ./pictures/image{i}.jpg')
+        os.system(f'cp wally.jpg ./pictures/image{i}.jpg')
 
-    sleep(1)
+        sleep(1)
 
-    os.system('./robot36')
+        os.system('./robot36')
 
-    sleep(2)
+        sleep(2)
 
-    mixer.music.load('wally.wav')
-    mixer.music.play()
-    while mixer.music.get_busy() == True:
-        continue
+        mixer.music.load('wally.wav')
+        mixer.music.play()
+        while mixer.music.get_busy() == True:
+            continue
 
-    i = i+1
+        i = i+1
+        
+        sleep(10)
     
-    sleep(10)
+    except KeyboardInterrupt:
+        break
+
+camera.close()
